8350b5144bf14b7a5ad90e2fbf9e32737da10e2f
Veronica Maya Santi <veronicamayasanti@gmail.com>
2024-05-24 13:16:18 +0700

    comiit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="main">
+      <Routes>
+        <Route path="/" element={<Login />}>
+          {/* <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+      </div>
+    </BrowserRouter>
+
+  
+      
+    
+  );
+}
+
+export default App;
---
+++ client/src/components/Login.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+function Login() {
+return (
+    <section className="sign-in">
+            <div className="container">
+                <div className="signin-content">
+
+                    <div className="signin-image">
+                        <figure><img src="images/signin-image.jpg" alt="sing up image" /></figure>
+                        <a href="/" className="signup-image-link">Create an account</a>
+                    </div>
+
+                    <div className="signin-form">
+                        <h2 className="form-title">Login</h2>
+                        <form method="POST" className="register-form" id="login-form">
+                            <div className="form-group">
+                                <label for="your_name"><i className="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="your_name" id="your_name" placeholder="Your Name"/>
+                            </div>
+
+                            <div className="form-group">
+                                <label for="your_pass"><i className="zmdi zmdi-lock"></i></label>
+                                <input type="password" name="your_pass" id="your_pass" placeholder="Password"/>
+                            </div>
+                            
+                            <div className="form-group form-button">
+                                <input type="submit" name="signin" id="signin" className="form-submit" value="Log in"/>
+                            </div>
+                        </form>
+                    
+                    </div>
+                </div>
+            </div>
+        </section>
+  )
+}
+
+export default Login
