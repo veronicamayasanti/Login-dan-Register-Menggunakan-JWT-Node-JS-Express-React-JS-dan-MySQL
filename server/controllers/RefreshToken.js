da764e297d250db44e2e16cc9e3ee5f3947ad926
Veronica Maya Santi <veronicamayasanti@gmail.com>
2024-06-07 16:49:51 +0700

    commit

--- server/controllers/RefreshToken.js
+++ server/controllers/RefreshToken.js
@@ -4,16 +4,20 @@ import jwt from "jsonwebtoken";
 export const refreshToken = async(req, res) => {
     try {
         const refreshToken = req.cookies.refreshToken;
-        console.log(refreshToken);
-        if(!refreshToken) return res.sendStatus(401);
+        console.log("ini refresh token", refreshToken);
+        if(!refreshToken) return res.sendStatus(401).json({msg: "tidak terdapat resfres token"});
         const user = await Users.findAll({
             where: {
                 refresh_token: refreshToken
             }
         });
-        if(!user[0]) return res.sendStatus(403);
+        if (!user[0]) return res.sendStatus(403).json({ msg: "token tidak cocok" });
+
+        //jika token cocok
         jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
-            if(err) return res.sendStatus(403);
+            if (err) return res.sendStatus(403).json({ msg: "gagal mencocokan token" });
+
+            // jika tidak error diambil value
             const userId = user[0].id;
             const name = user[0].name;
             const email = user[0].email;
--- server/middleware/verifyToken.js
+++ server/middleware/verifyToken.js
@@ -5,7 +5,7 @@ export const verifyToken = (req, res, next) => {
     const token = authHeader && authHeader.split(' ')[1];
     if (token == null) return res.sendStatus(401);
 
-    jwt.verify(token, "bvejdgheiugthihdkbxcdskjhgirehgifcdkklsjroegtksghuergh", (err, decoded) => {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) return res.sendStatus(403);
         req.email = decoded.email;   // nama variable di dalam decode
         next();
