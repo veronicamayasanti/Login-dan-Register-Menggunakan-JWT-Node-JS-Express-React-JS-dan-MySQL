4985ab73ad95fa5671f6b4ae4d2320a81e58d67f
Veronica Maya Santi <veronicamayasanti@gmail.com>
2024-08-10 13:30:31 +0700

    fix dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -5,8 +5,9 @@ import { useNavigate } from 'react-router-dom';
 
 
 const Dashboard = () => {
-  const [name, setName] = useState([]);
+  const [name, setName] = useState('');
   const [token, setToken] = useState('');
+  const [users, setUsers] = useState([]);
   const [expire, setExpire] = useState('');
 
   const navigate = useNavigate();
@@ -20,12 +21,9 @@ const Dashboard = () => {
   const refreshToken = async () => {
     try {
       const response = await axios.get('http://localhost:8080/token');
-      setToken(response.data.accessToken);
+      setToken(response.data.accesToken);
+      const decoded = jwtDecode(response.data.accesToken);
       setName(response.data.name);
-      console.log(response.data);
-
-      const decoded = jwtDecode(response.data.accessToken);
-      console.log(decoded);
       setExpire(decoded.exp);
     } catch (error) {
       if (error.response) {
@@ -34,20 +32,60 @@ const Dashboard = () => {
     }
   };
 
-
+  const axiosJWT = axios.create();
+  axiosJWT.interceptors.request.use(async (config) => {
+    const currentDate = new Date();
+    if (expire * 1000 < currentDate.getTime()) {
+      const response = await axios.get('http://localhost:8080/token');
+      config.headers.Authorization = `Bearer ${response.data.accesToken}`;
+      setToken(response.data.accesToken);
+      const decoded = jwtDecode(response.data.accesToken);
+      setName(decoded.name);
+      setExpire(decoded.exp);
+    }
+    return config;
+  }, (error) => {
+    return Promise.reject(error);
+  });
+    
+  
   const getUsers = async () => {
-    const response = await axios.get('http://localhost:8080/users', {
+    const response = await axiosJWT.get('http://localhost:8080/users', {
       headers: {
         Authorization: `Bearer ${token}`,
       },
+      
     });
-    console.log(response.data);
+    setUsers(response.data);
   }
 
   return (
     <div>
       <h1>Welcome {name}</h1>
       <button onClick={getUsers}>Get Users</button>
+      <table border="1" cellPadding="10" className='table'>
+   <thead>
+     <tr>
+      <th>No</th>
+       <th>Name</th>
+       <th>Email</th>
+     </tr>
+   </thead>
+   <tbody>
+     {users.map(user => (
+       <tr key={user.id}>
+         <td>{user.id}</td>
+         <td>{user.name}</td>
+         <td>{user.email}</td>
+       </tr>
+     ))}
+   </tbody>
+      </table>
+
+    
+     
+
+    
     </div>
   )
 }
